c41fceb590b4e47a67359b8e25bfe64348daad08
Ryan Griffith <ryan.griffith@hannonhill.com>
2018-10-04 15:07:15 -0400

    Updated based on latest iteration used in Cascade CMS Cloud
    - Adds additional application level logging for debugging
    - Removes `@domain` from authenticating user value to handle email addressess
    - Optional code for forcing lowercase usernames

--- Shibboleth/ShibbAuthentication.java
+++ Shibboleth/ShibbAuthentication.java
@@ -1,4 +1,15 @@
-package com.hannonhill.cascade.shibb;
+/**
+ * This is based on the work of Andre Daniels (andre777 [at] ucsc [dot] edu) from UC Santa Cruz.
+ *
+ * @author Bradley Wagner
+ * @author Andre Daniels
+ * @author Ryan Griffith
+ *
+ * @description This is an implementation of the Cascade Server Authentication module that requires the user
+ *              login via Shibboleth.
+ * @see https://github.com/hannonhill/Custom-Authentication-Module-Examples/tree/master/Shibboleth
+ */
+package com.hannonhill.cascade.auth;
 
 import java.io.IOException;
 
@@ -10,32 +21,21 @@ import org.apache.log4j.Logger;
 import com.hannonhill.cascade.model.security.authentication.AuthenticationPhase;
 import com.hannonhill.cascade.model.security.authentication.Authenticator;
 
-/**
- * This is based on the work of Andre Daniels (andre777 [at] ucsc [dot] edu) from UC Santa Cruz.
- * 
- * @author Bradley Wagner
- * @author Andre Daniels
- * 
- * @description This is an implementation of the Cascade Server Authentication module that requires the user
- *              login via Shibboleth.
- */
-public class ShibbAuthentication implements Authenticator
+public class CascadeShibAuth implements Authenticator
 {
-    private static Logger logger = Logger.getLogger("shib-log");
-    
-    /**
-     * Logs a message as an ERROR. Useful for debugging as ERRORS show up in the log by default.
-     */
+    private static Logger logger = Logger.getLogger(CascadeShibAuth.class);
+    private static String LOGOUT_URL = "https://";
+
     private void log(String message)
     {
-        logger.error(message);
+        logger.debug(message);
     }
 
     /**
      * Determines if the user should be redirected to a particular place during LOGIN or LOGOUT
-     * 
+     *
      * Use the reseponse to set a redirect location.
-     * 
+     *
      * @param request
      * @param response
      * @param authenticationPhase Indicates whether the user is logging in or logging out
@@ -49,29 +49,37 @@ public class ShibbAuthentication implements Authenticator
         // This is useful for creating a backdoor into Cascade using a different port
         // or virtual host in Apache that does not require a Shibboleth session
         if (request.getAttribute("uid") == null)
+        {
+            log("No request attribute, uid, was found. Redirecting to Cascade CMS login screen.");
             return false;
+          }
+
+        log("Request attribute, uid, found. Processing authenticationPhase...");
 
         switch (authenticationPhase)
         {
-        case LOGIN:
-            // Redirect to the custom authentication servlet so that it can call this classes' authenticate()
-            response.sendRedirect(Authenticator.AUTHENTICATION_URI);
-            return true;
-        case LOGOUT:
-            // Shibboleth doesn't support Single Sign-Out but this will ensure that 
-            // the local Shibboleth session is destroyed and that the system will have
-            // to go back to the Shibboleth IdP to ensure the user is still logged in.
-            response.sendRedirect("/Shibboleth.sso/Logout");
-            return true;
-        default:
-            // should never get here
-            return false;
+            case LOGIN:
+                log("Redirecting to custom auth servlet for authentication");
+                // Redirect to the custom authentication servlet so that it can call this classes' authenticate()
+                response.sendRedirect(Authenticator.AUTHENTICATION_URI);
+                return true;
+            case LOGOUT:
+                log("Redirecting to custom logout URL: ", LOGOUT_URL);
+                // Shibboleth doesn't support Single Sign-Out but this will ensure that
+                // the local Shibboleth session is destroyed and that the system will have
+                // to go back to the Shibboleth IdP to ensure the user is still logged in.
+                response.sendRedirect(LOGOUT_URL);
+                return true;
+            default:
+                // should never get here
+                log("How did we get here!?");
+                return false;
         }
     }
 
     /**
      * Allows the module to report back whether or not the user has been authenticated.
-     * 
+     *
      * @param request
      * @param response
      * @return the username if the user successfully authenticated
@@ -79,9 +87,22 @@ public class ShibbAuthentication implements Authenticator
     public String authenticate(HttpServletRequest request, HttpServletResponse response)
     {
         // Pull the appropriate attribute passed to it by Apache/AJP and return it
-        
         log("Attribute: uid, Value: " + request.getAttribute("uid"));
         String username = (String) request.getAttribute("uid");
+
+        int stripIndex = username.indexOf("@");
+        if (stripIndex != -1)
+        {
+            log("Found @ symbol at index: " + stripIndex);
+            String removed = username.substring(stripIndex);
+    		log("Removing " + removed + " from uid value");
+            username = username.substring(0, stripIndex);
+        }
+
+        // Optional if forcing lowercase
+        //log("Forced lowercase is enabled, lowercasing " + username + " to " + username.toLowerCase());
+        //username = username.toLowerCase();
+
         log("Authenticating with username: " + username);
 
         return username;
